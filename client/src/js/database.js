8c8ec8ecfee223421fc7dd9e4905676abb6e3ec2
Nihsadd <nihsadd@gmail.com>
2024-06-08 18:48:33 -0600

    add database todos

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,46 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+
+  //open connection to the database
+  const db = await openDB('jate', 1);
+
+  //create a new transaction and specify the database and data privileges
+  const tx = db.transaction('jate', 'readwrite');
+
+  // open the desired object store
+  const store = tx.objectStory('jate');
+
+  //add or update the content in the database
+  const request = store.put({ id: 1, content: content });
+
+  //get confirmation of the request
+  const result = await request;
+  console.log('data saved to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from the database');
+
+  //open a connection to the database
+  const db = await openDB('jate', 1);
+
+  //oreate a new transaction and specify the database and data privileges
+  const tx = db.transaction('jate', 'readonly');
+
+  //open the desired object store
+  const store = tx.objectStore('jate');
+
+  //oet all data in the database
+  const request = store.getAll();
+
+  //oet confirmation of the request
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
